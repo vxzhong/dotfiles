f27eebb17f920966a1ad635776e20726a63b65c7
zhang zhong <vxzhong@qq.com>
2016-08-26 14:44:02 +0800

    update

---
+++ atom/apm.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+packages = [
+  'autocomplete-python',
+  'file-icons',
+  'minimap',
+  'pigments',
+  'linter'
+]
+
+installedPackage = subprocess.run(
+  ['apm', 'list', '-i'], stdout=subprocess.PIPE, universal_newlines=True).stdout
+
+needToInstallPackage = []
+
+for p in packages:
+  if p in installedPackage:
+    pass
+  else:
+    needToInstallPackage.append(p)
+
+
+installCmd = ['apm', 'install']
+installCmd.extend(needToInstallPackage)
+
+subprocess.run(installCmd, check=True);
